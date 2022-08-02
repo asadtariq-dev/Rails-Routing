d66bfac281605f34850c420e6a6b205939860751
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-02 18:21:06 +0500

    Changed the routes to required ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  get 'products', to: 'products#index', as: 'products'
+  get 'products/:id', to: 'products#show', as: 'product'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
