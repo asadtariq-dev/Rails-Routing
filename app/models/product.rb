a82f50cda0307a321fbe448a8819e702072ac141
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-03 17:41:14 +0500

    Added after_destroy_commit Callback to the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,15 @@ class Product < ApplicationRecord
   validates :price, numericality: true
   validates :name, uniqueness: true
   after_save_commit :log_product_saved
+  after_destroy_commit :log_product_destroyed
 
   private
 
   def log_product_saved
-    puts 'Product saved to database'
+    puts "=============\nProduct saved from the database\n============="
+  end
+
+  def log_product_destroyed
+    puts "=============\nProduct deleted from the database\n============="
   end
 end
