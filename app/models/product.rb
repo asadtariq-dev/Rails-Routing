6c178faf5ec8b968722fbe92e100a8688798ddf4
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-03 17:33:32 +0500

    Added after_save_commit Callback to the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,11 @@ class Product < ApplicationRecord
   validates :name, :price, presence: true
   validates :price, numericality: true
   validates :name, uniqueness: true
+  after_save_commit :log_product_saved
 
+  private
+
+  def log_product_saved
+    puts 'Product saved to database'
+  end
 end
