1cbc36ce34f0005adb0122ae7dd57ef45553e334
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-04 18:28:25 +0500

    Added route + action + view for new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,28 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  # Creates an object of product without saving it
+  def new
+    @product = Product.new
+  end
+
+  # Creates an product with handling the success and error condition
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = 'Product successfully created'
+      redirect_to @product
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new'
+    end
+  end
+
+  private
+
+  # Filters params using Strong Parameters
+  def product_params
+    params.require(:products).permit(:name, :description, :image_url, :price)
+  end
 end
