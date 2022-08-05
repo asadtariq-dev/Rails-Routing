b2ae3b10e77669e7717ac8059d4fc43ae6d9eab6
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-06 01:27:47 +0500

    Added a helper for error fetching from product

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,20 +1,5 @@
 module ProductsHelper
-    def analyze_price(product)
-      if product.price > 30
-        "Expensive"
-      else
-        "Cheap"
-      end
-    end
-
-    def show_errors(object, field_name)
-      puts object.name
-      puts "\n\n\n\n\n\n\n\n\n\n\ndg\n\nas\nd\nufgadga\n\n\n\n\n\n\n\n\n"
-      if object.errors.any?
-        puts "error"
-        if !object.errors.messages[field_name].blank?
-          puts object.errors.messages[field_name].join(", ")
-        end
-      end
-    end
+  def show_errors(object, field)
+    object.errors[field] unless object.errors.blank?
+  end
 end
