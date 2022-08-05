81505aaa41505f9bb174b9d4a040b78712b0c82d
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-05 06:18:53 +0500

    added show error helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -6,4 +6,15 @@ module ProductsHelper
         "Cheap"
       end
     end
+
+    def show_errors(object, field_name)
+      puts object.name
+      puts "\n\n\n\n\n\n\n\n\n\n\ndg\n\nas\nd\nufgadga\n\n\n\n\n\n\n\n\n"
+      if object.errors.any?
+        puts "error"
+        if !object.errors.messages[field_name].blank?
+          puts object.errors.messages[field_name].join(", ")
+        end
+      end
+    end
 end
