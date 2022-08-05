d438f805d80c6b6dd0b5c3a7e4ab815563fcbb2a
asadtariq-dev <asad.tariq@devsinc.com>
2022-08-05 06:17:38 +0500

    Added action, form and view for creating product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'products#index'
-  resources :products, only: %i[index show new destroy]
+  resources :products, only: %i[index show new create destroy]
 
   # get 'products', to: 'products#index', as: 'products'
   # get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
